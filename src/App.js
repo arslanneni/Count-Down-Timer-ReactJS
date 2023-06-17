15648a8ba4a46290486af896c9617799d2ed6534
Arslan Ali <arslan.neni23@gmail.com>
2023-06-17 14:32:47 +0500

    files uploaded

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import AddDate from './components/AddDate';
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+    <AddDate/>
+    </div>
+   
+  );
+}
+
+export default App;
